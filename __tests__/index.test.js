9b9a3962bd3f49949097b2450dbc93ec2378a2a9
Patrick Ethier <patrick@secureops.com>
2019-07-22 15:28:02 +0200

    Upload initial unit testing to see if it results in coverage

---
+++ __tests__/index.test.js
@@ -0,0 +1,54 @@
+const lambda = require('../index');
+
+const context = {};
+const privateEvent = {
+  "Records": [
+    {
+      "cf": {
+        "config": {
+          "distributionId": "EXAMPLE"
+        },
+        "request": {
+          "uri": "/index.html",
+          "method": "GET",
+        }
+      }
+    }
+  ]
+}
+
+const publicEvent = {
+  "Records": [
+    {
+      "cf": {
+        "config": {
+          "distributionId": "EXAMPLE"
+        },
+        "request": {
+          "uri": "/public/index.css",
+          "method": "GET",
+        }
+      }
+    }
+  ]
+}
+
+process.env.WHITELIST= '["/public/*"]';
+
+test('Check if a directory matches whitelist', done => {
+  function callback(something, data) {
+    console.log(data);
+    expect(data.statusCode).toBe(401);
+    done();
+  }
+  lambda.handler(privateEvent, context, callback);
+});
+
+test('Check if a directory doesn\'t match whitelist', done => {
+  function callback(something, data) {
+    console.log(data);
+    expect(data.statusCode).toBe(publicEvent.Records[0].request);
+    done();
+  }
+  lambda.handler(publicEvent, context, callback);
+});
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Comment
 exports.handler = async (event, context, callback) => {
-    console.log(event);
     const cfrequest = event.Records[0].cf.request;
     const headers = cfrequest.headers;
     const uri = cfrequest.uri;
     let whitelist = [];
     if (process.env.WHITELIST) {
+      console.log(process.env.WHITELIST);
       whitelist = JSON.parse(process.env.WHITELIST);
     }
-    console.log(whitelist);
     whitelist.forEach(item => {
         var re = new RegExp(item);
         if (re.test(uri)) {
