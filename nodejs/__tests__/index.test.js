bd0b6e3744fce11b22cf4a2bac7cc29f7aa7df07
Patrick Ethier <patrick@secureops.com>
2019-07-31 20:13:53 +0200

    Add new functionality that recognizes JWT tokens in param/headers/cookies. Also fix so that the default authorization response redirects to the root as we want to avoid a 302 and just return index.html with the cookie set

--- nodejs/__tests__/index.test.js
+++ nodejs/__tests__/index.test.js
@@ -1,55 +1,401 @@
+'use strict';
 // Execute tests to verify that whitelists are working
 const lambda = require('../index');
+const env = require('../env');
+const util = require('util');
+const fetchMock = require('node-fetch');
+const jsonwebtoken = require('jsonwebtoken');
 
 const context = {};
-const privateEvent = {
-  "Records": [
-    {
-      "cf": {
-        "config": {
-          "distributionId": "EXAMPLE"
+const mockKeys = { COGNITO_JWKS: {"somekey": "somekey"}};
+
+beforeEach(() => {
+  fetchMock.reset();
+})
+
+test('viewer-response without a new authorization header in request', (done) =>{
+  const viewerResponse = {
+    Records: [{
+      cf: {
+        config: {
+          distributionDomainName: 'd7123456.cloudfront.net',
+          distributionId: 'E0000000',
+          eventType: 'viewer-response',
+          requestId: 'abcdef0123456789=='
         },
-        "request": {
-          "uri": "/index.html",
-          "method": "GET",
+        request: {
+          clientIp: '1.1.1.1',
+          headers: {
+            host: [{
+              key: 'Host',
+              value: 'my.example.site'
+            }],
+            referer: [{
+              key: 'Referer',
+              value: 'https://myidp.auth.us-east-1.amazoncognito.com/login?client_id=abcdef0123456789&redirect_uri=https://my.example.site/&response_type=code&scope=openid+email'
+            }]
+          },
+          method: 'GET',
+          querystring: 'code=abcdef0123456789',
+          uri: '/'
+        },
+        response: {
+          headers: {
+            'accept-ranges': [{
+              key: 'Accept-Ranges',
+              value: 'bytes'
+            }],
+            server: [{
+              key: 'Server',
+              value: 'AmazonS3'
+            }],
+            age: [{
+              key: 'Age',
+              value: '17016'
+            }],
+            'content-type': [{
+              key: 'Content-Type',
+              value: 'text/html'
+            }],
+            'content-length': [{
+              key: 'Content-Length',
+              value: '2045'
+            }]
+          },
+          status: '200',
+          statusDescription: 'OK'
         }
       }
-    }
-  ]
-}
+    }]
+  };
 
-const publicEvent = {
-  "Records": [
-    {
-      "cf": {
-        "config": {
-          "distributionId": "EXAMPLE"
+  const expected_result = {
+  "headers": {
+    "accept-ranges": [{
+      "key": "Accept-Ranges",
+      "value": "bytes"
+    }],
+    "age": [{
+      "key": "Age",
+      "value": "17016"
+    }],
+    "content-length": [{
+      "key": "Content-Length",
+      "value": "2045"
+    }],
+    "content-type": [{
+      "key": "Content-Type",
+      "value": "text/html"
+    }],
+    "server": [{
+      "key": "Server",
+      "value": "AmazonS3"
+    }]
+  },
+  "status": "200",
+  "statusDescription": "OK"
+};
+
+  function callback(err, data) {
+    expect(err).toBeNull();
+    expect(data).toEqual(expected_result);
+    done();
+  }
+
+  lambda.handler(viewerResponse, context, callback);
+});
+
+test('viewer-response with a new authorization header in request', (done) =>{
+  var payload = { iat: Math.floor(Date.now() / 1000 ), token_use: "access" };
+  var header = { kid: 'somekey' };
+  const mytoken = jsonwebtoken.sign(payload, 'somekey', { header: header});
+
+  const viewerResponse = {
+    Records: [{
+      cf: {
+        config: {
+          distributionDomainName: 'd7123456.cloudfront.net',
+          distributionId: 'E0000000',
+          eventType: 'viewer-response',
+          requestId: 'abcdef0123456789=='
         },
-        "request": {
-          "uri": "/public/index.css",
-          "method": "GET",
+        request: {
+          clientIp: '1.1.1.1',
+            headers: {
+              host: [{
+                key: 'Host',
+                value: 'my.example.site'
+              }],
+              'user-agent': [{
+                key: 'User-Agent',
+                value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0'
+              }],
+              referer: [{
+                key: 'Referer',
+                value: 'https://myidp.auth.us-east-1.amazoncognito.com/login?client_id=abcdef0123456789&redirect_uri=https://my.example.site/&response_type=code&scope=openid+email'
+              }],
+              newauthtoken: [{
+                key: 'newauthtoken',
+                value: mytoken
+              }]
+          },
+          method: 'GET',
+          querystring: 'code=abcdef0123456789',
+          uri: '/'
+        },
+        response: {
+          headers: {
+            'accept-ranges': [{
+              key: 'Accept-Ranges',
+              value: 'bytes'
+            }],
+            server: [{
+              key: 'Server',
+              value: 'AmazonS3'
+            }],
+            age: [{
+              key: 'Age',
+              value: '17016'
+            }],
+            'content-type': [{
+              key: 'Content-Type',
+              value: 'text/html'
+            }],
+            'content-length': [{
+              key: 'Content-Length',
+              value: '2045'
+            }]
+          },
+          status: '200',
+          statusDescription: 'OK'
         }
       }
+    }]
+  };
+
+  const expected_result = {
+  "headers": {
+    "accept-ranges": [{
+      "key": "Accept-Ranges",
+      "value": "bytes"
+    }],
+    "age": [{
+      "key": "Age",
+      "value": "17016"
+    }],
+    "content-length": [{
+      "key": "Content-Length",
+      "value": "2045"
+    }],
+    "content-type": [{
+      "key": "Content-Type",
+      "value": "text/html"
+    }],
+    "server": [{
+      "key": "Server",
+      "value": "AmazonS3"
+    }],
+    "set-cookie": [{
+      "key": "Set-Cookie",
+      "value": "sessiontoken=" + mytoken + ";Path=/;Secure"
+    }]
+  },
+  "status": "200",
+  "statusDescription": "OK"
+};
+
+  function callback(err, data) {
+    expect(err).toBeNull();
+    expect(data).toEqual(expected_result);
+    done();
+  }
+
+  lambda.handler(viewerResponse, context, callback);
+});
+
+test('regular request with authorization header', (done) => {
+  var payload = { iat: Math.floor(Date.now() / 1000 ), token_use: "access" };
+  var header = { kid: 'somekey' };
+  const mytoken = jsonwebtoken.sign(payload, 'somekey', { header: header});
+  console.log(jsonwebtoken.decode(mytoken, {complete: true }));
+
+  const requestEvent = {
+    "Records": [
+      {
+        "cf": {
+          config: {
+            distributionDomainName: 'd7123456.cloudfront.net',
+            distributionId: 'E0000000',
+            eventType: 'viewer-request',
+            requestId: 'abcdef0123456789=='
+          },
+          "request": {
+            "headers": {
+              "referer": [{
+                "key": "Referer",
+                "value": "https://myidp.auth.us-east-1.amazoncognito.com/login?client_id=123456&redirect_uri=https://mywebsite.example.com/&response_type=code&scope=openid+email"
+              }]
+            },
+            "querystring": "authorization=" + mytoken,
+            "uri": "/",
+            "method": "GET"
+          }
+        }
+      }
+    ]
+  };
+
+  env.COGNITO_JWKS = { "somekey": "somekey" };
+  function callback(err, data) {
+    expect(err).toBeNull();
+    expect(data).toEqual(requestEvent.Records[0].cf.request);
+    done();
+  }
+
+  lambda.handler(requestEvent, context, callback);
+});
+
+test('regular request with cookie', (done) => {
+  var payload = { iat: Math.floor(Date.now() / 1000 ), token_use: "access" };
+  var header = { kid: 'somekey' };
+  const mytoken = jsonwebtoken.sign(payload, 'somekey', { header: header});
+  console.log(jsonwebtoken.decode(mytoken, {complete: true }));
+
+  const requestEvent = {
+  Records: [{
+    cf: {
+      config: {
+        distributionDomainName: 'd7123456.cloudfront.net',
+        distributionId: 'E0000000',
+        eventType: 'viewer-request',
+        requestId: 'abcdef0123456789=='
+      },
+      request: {
+        clientIp: '1.1.1.1',
+        headers: {
+          host: [{
+            key: 'Host',
+            value: 'my.example.site'
+          }],
+          referer: [{
+            key: 'Referer',
+            value: 'https://myidp.auth.us-east-1.amazoncognito.com/login?client_id=abcdef0123456789&redirect_uri=https://my.example.site/&response_type=code&scope=openid+email'
+          }],
+          cookie: [{
+            key: 'Cookie',
+            value: 'sessiontoken=' + mytoken
+          }]
+        },
+        method: 'GET',
+        querystring: '',
+        uri: '/static/js/main.1234567.chunk.js'
+      }
     }
-  ]
-}
+  }]
+};
+  function callback(err, data) {
+    expect(err).toBeNull();
+    expect(data).toEqual(requestEvent.Records[0].cf.request);
+    done();
+  }
 
-process.env.WHITELIST= '["/public/*"]';
+  lambda.handler(requestEvent, context, callback);
+});
 
-test('Check if a directory matches whitelist', done => {
-  function callback(something, data) {
-    console.log(data);
-    expect(data.statusCode).toBe(401);
+test('token response path with a valid token response', (done) => {
+  const redirected_from_idp = {
+    Records: [
+      {
+        cf: {
+          config: {
+            distributionDomainName: 'd7123456.cloudfront.net',
+            distributionId: 'E0000000',
+            eventType: 'viewer-request',
+            requestId: 'abcdef0123456789=='
+          },
+          request: {
+            clientIp: '1.1.1.1',
+            headers: {
+              host: [{
+                key: 'Host',
+                value: 'my.example.site'
+              }],
+              referer: [{
+                key: 'Referer',
+                value: 'https://myidp.auth.us-east-1.amazoncognito.com/login?client_id=abcdef0123456789&redirect_uri=https://my.example.site/&response_type=code&scope=openid+email'
+              }],
+            },
+            querystring: "code=abcdef0123456789",
+            uri: env.OAUTH_AUTH_RESPONSE,
+            method: "GET"
+          }
+        }
+      }
+    ]
+  };
+  const token_response = { body: { access_token: "abcdef0123456" }, status: 200};
+  fetchMock.post("begin:" + env.COGNITO_DOMAIN, token_response);
+
+  const expected_response = {
+    clientIp: '1.1.1.1',
+    headers: {
+      host: [{
+        key: 'Host',
+        value: 'my.example.site'
+      }],
+      referer: [{
+        key: 'Referer',
+        value: 'https://myidp.auth.us-east-1.amazoncognito.com/login?client_id=abcdef0123456789&redirect_uri=https://my.example.site/&response_type=code&scope=openid+email'
+      }],
+      newauthtoken: [{
+        key: 'newauthtoken',
+        value: 'abcdef0123456'
+      }]
+    },
+    querystring: 'code=abcdef0123456789',
+    uri: '/',
+    method: 'GET'
+  };
+
+  function callback(err, data) {
+    console.log(util.inspect(data, {
+      showHidden: false,
+      depth: null
+    }));
+    expect(err).toBeNull();
+    expect(data).toEqual(expected_response);
+    done();
+  }
+  lambda.handler(redirected_from_idp, context, callback);
+});
+
+test('Ask for an auth protected file and return login redirect', (done) => {
+  const privateEvent = {
+    "Records": [
+      {
+        "cf": {
+          config: {
+            distributionDomainName: 'd7123456.cloudfront.net',
+            distributionId: 'E0000000',
+            eventType: 'viewer-request',
+            requestId: 'abcdef0123456789=='
+          },
+          "request": {
+            "uri": "/index.html",
+            "method": "GET",
+          }
+        }
+      }
+    ]
+  };
+  function callback(err, data) {
+    expect(err).toBeNull();
+    console.log(util.inspect(data, {
+      showHidden: false,
+      depth: null
+    }));
+    expect(data.status).toBe(302);
     done();
   }
   lambda.handler(privateEvent, context, callback);
-});
-
-test('Check if a directory doesn\'t match whitelist', done => {
-  function callback(something, data) {
-    console.log(data);
-    expect(data.statusCode).toBe(publicEvent.Records[0].request);
-    done();
-  }
-  lambda.handler(publicEvent, context, callback);
 });
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,29 +1,198 @@
 'use strict';
-var jwt = require('jsonwebtoken');
-var jwkToPem = require('jwk-to-pem');
+const util = require('util');
+const fetch = require('node-fetch');
+const { Headers } = require('node-fetch');
+const {
+  URLSearchParams
+} = require('url');
+const querystring = require('querystring');
+const env = require('./env');
+const jwtCheck = require('./jwt');
 
-
-exports.handler = async (event, context, callback) => {
-    const cfrequest = event.Records[0].cf.request;
-    const headers = cfrequest.headers;
-    const uri = cfrequest.uri;
-    let whitelist = [];
-    if (process.env.WHITELIST) {
-      console.log(process.env.WHITELIST);
-      whitelist = JSON.parse(process.env.WHITELIST);
+// From Michael - sqlbot on StackOverFlow
+// Taken from article: https://stackoverflow.com/questions/55128624/cloudfront-lambdaedge-set-cookie-on-viewer-request
+function extract_cookie(headers, cname) {
+    const cookies = headers['cookie'];
+    if(!cookies)
+    {
+        console.log("extract_cookie(): no 'Cookie:' headers in request");
+        return null;
     }
-    whitelist.forEach(item => {
-        var re = new RegExp(item);
-        if (re.test(uri)) {
-            callback(null, cfrequest)
-            return true;
-        }
-    });
 
-    const response = {
-        statusCode: 401,
-        statusDescription: 'Unauthorized'
-    };
-    callback(null, response)
-    return response;
+    // iterate through each Cookie header in the request, last to first
+
+    for (var n = cookies.length; n--;)
+    {
+        // examine all values within each header value, last to first
+
+        const cval = cookies[n].value.split(/;\ /);
+        const vlen = cval.length;
+
+        for (var m = vlen; m--;)
+        {
+            const cookie_kv = cval[m].split('=');
+            if(cookie_kv[0] === cname)
+            {
+                return cookie_kv[1];
+            }
+        } // for m (each value)
+    } // for n (each header)
+
+    // we have no match if we reach this point
+    console.log('extract_cookie(): cookies were found, but the specified cookie is absent');
+    return null;
+}
+
+
+exports.handler = (event, context, lambda_return_cb) => {
+  console.log(util.inspect(event, {
+    showHidden: false,
+    depth: null
+  }));
+  const cfresponse = event.Records[0].cf.response;
+  const cfrequest = event.Records[0].cf.request;
+
+  // Check if this is a response event
+  if (event.Records[0].cf.config.eventType === 'viewer-response') {
+    console.log('Processing a response');
+    if (cfrequest.headers.newauthtoken) {
+      console.log("Processing a response that needs a cookie set to newauthtoken");
+      const access_token = cfrequest.headers.newauthtoken[0].value;
+      // Set the auth Cookie
+      cfresponse.headers["set-cookie"] = [
+        {
+          "key": "Set-Cookie",
+          "value": "sessiontoken=" + access_token + ";Path=/;Secure"
+        }
+      ];
+      console.log("Returning the cookie with " + util.inspect(cfresponse, {
+        showHidden: false,
+        depth: null
+      }));
+    }
+    lambda_return_cb(null, cfresponse);
+    return true;
+  }
+
+  const headers = cfrequest.headers;
+  const uri = cfrequest.uri;
+  const queryString = cfrequest.querystring;
+  var authToken = null;
+
+  const redirect_response = {
+    status: 302,
+    statusDescription: 'Please authenticate',
+    headers: {
+      'location': [{
+        "value": env.COGNITO_DOMAIN + "/login" +
+          "?client_id=" + env.COGNITO_CLIENT_ID +
+          "&redirect_uri=" + env.REDIRECT_URI + env.OAUTH_AUTH_RESPONSE +
+          "&response_type=code&scope=openid+email"
+      }]
+    }
+  };
+
+  console.log(uri);
+  if (queryString) {
+    console.log("QueryString detected: " + util.inspect(queryString, {
+      showHidden: false,
+      depth: null
+    }));
+    const qsItems = querystring.parse(queryString);
+    if (qsItems.code) {
+      console.log("Code detected: " + qsItems.code);
+      const params = new URLSearchParams();
+      params.append('grant_type', 'authorization_code');
+      params.append('client_id', env.COGNITO_CLIENT_ID);
+      params.append('redirect_uri', env.REDIRECT_URI + env.OAUTH_AUTH_RESPONSE);
+      params.append('code', qsItems.code);
+      const headers = {
+        Authorization: 'Basic ' +
+          Buffer.from(env.COGNITO_CLIENT_ID + ":" + env.COGNITO_CLIENT_SECRET).toString('base64')
+      };
+
+      return fetch(env.COGNITO_DOMAIN + '/oauth2/token', {
+          method: 'POST',
+          body: params,
+          headers: headers
+        })
+        .then(res => res.json())
+        .then(json => {
+          console.log("authorization_response received json: " +
+            util.inspect(json, {
+              showHidden: false,
+              depth: null
+            })
+          );
+          cfrequest.headers.newauthtoken = [
+            {
+              key: "newauthtoken",
+              value: json.access_token
+            }
+          ];
+          cfrequest.uri = "/";
+          lambda_return_cb(null, cfrequest);
+          return true;
+        })
+        .catch(error => {
+          console.log("Error fetching auth code: " + util.inspect(error, {
+            showHidden: false,
+            depth: null
+          }));
+          lambda_return_cb(null, redirect_response);
+          return true;
+        });
+    }
+    else if(qsItems.authorization) {
+      console.log("Authorization parameter matched");
+      authToken = qsItems.authorization;
+    }
+  }
+  if(cfrequest.headers) {
+    if(authToken === null) {
+      authToken = extract_cookie(cfrequest.headers, 'sessiontoken');
+    }
+    if (authToken === null) {
+    // Check for the authorization header
+      if (cfrequest.headers.authorization) {
+        console.log("Authorization Header present: " +
+          util.inspect(cfrequest.headers.authorization, {
+            showHidden: false,
+            depth: null
+          })
+        );
+        authToken = cfrequest.headers.authorization;
+      }
+    }
+  }
+  if (authToken) {
+    return jwtCheck.verifyToken(authToken, (err, data) =>{
+      if (err) {
+        console.log("Authorization required")
+        lambda_return_cb(null, redirect_response);
+        console.log(util.inspect(redirect_response, {
+          showHidden: false,
+          depth: null
+        }));
+        return true;
+      }
+      else {
+        console.log("Authorization Success")
+        lambda_return_cb(null, cfrequest);
+        console.log(util.inspect(cfrequest, {
+          showHidden: false,
+          depth: null
+        }));
+        return true;
+      }
+    });
+  }
+
+  console.log("Authorization required")
+  lambda_return_cb(null, redirect_response);
+  console.log(util.inspect(redirect_response, {
+    showHidden: false,
+    depth: null
+  }));
+  return true;
 };
